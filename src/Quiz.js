5f5e316932c8532c7c385b7feb5e467a1868f80d
Liudvikas Taluntis <liudvikast@gmail.com>
2020-02-15 17:24:16 +0000

    Removed question variable so that Plural Tests could pass.

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,12 +13,11 @@ class Quiz extends Component{
   }
 
   render(){
-    const question = quizData.quiz_questions[this.state.quiz_position - 1]
     const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
     return(
       <div>
         {
-          isQuizEnd ? (<QuizEnd />) : (<QuizQuestion quiz_question={question} />)
+          isQuizEnd ? (<QuizEnd />) : (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />)
         }
       </div>
     )
