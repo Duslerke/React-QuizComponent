1655dfa75e2654b76ac5c5ad1a5a1fa6c3b8ac91
Liudvikas Taluntis <liudvikast@gmail.com>
2020-02-15 17:33:07 +0000

    Added button map logic

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,7 @@ class QuizQuestion extends Component{
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map((option, index) => <QuizQuestionButton key={index} button_text={option}/>)}
           </ul>
         </section>
       </main>
